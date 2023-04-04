c61bf6ee00c52e12225047d9f21c83faab63ae41
ZawLwinTunSCM <scm.zawlwitun@gmail.com>
2023-04-04 15:12:08 +0630

    create profile view

--- ems_mobile/lib/Screens/Common/common_widget.dart
+++ ems_mobile/lib/Screens/Common/common_widget.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+
 typedef EyeCallback = Function();
+
 class CommonWidget {
-  
   static InputDecoration loginInputDecoration(String name) => InputDecoration(
       hintText: name,
       labelText: name,
       contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
       border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)));
-  static InputDecoration passwordInputDecoration(String name, Icon icon, EyeCallback callback) =>
+  static InputDecoration passwordInputDecoration(
+          String name, Icon icon, EyeCallback callback) =>
       InputDecoration(
           hintText: name,
           labelText: name,
-          suffixIcon: IconButton(icon: icon,onPressed: callback,),
+          suffixIcon: IconButton(
+            icon: icon,
+            onPressed: callback,
+          ),
           contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)));
   static ButtonStyle primaryButtonStyle() => ElevatedButton.styleFrom(
@@ -25,4 +30,41 @@ class CommonWidget {
       padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
       backgroundColor: const Color.fromARGB(255, 224, 224, 224),
       foregroundColor: Colors.black);
+  static Padding profileRow(String left, String right) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  left,
+                  textAlign: TextAlign.left,
+                  style: profileTextStyle(Colors.green),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  right,
+                  textAlign: TextAlign.right,
+                  style: profileTextStyle(Colors.black54),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+  static Text profileTitle(String title) => Text(
+        title,
+        style: const TextStyle(
+            fontSize: 14, color: Colors.green, fontWeight: FontWeight.bold),
+      );
+  static TextStyle profileTextStyle(Color color) => TextStyle(
+        fontSize: 14,
+        color: color,
+      );
 }
