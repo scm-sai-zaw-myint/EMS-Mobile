0b25d1489ea7c275e4ea70ddb4bb5a29a8835172
Sai-Zaw-Myint <109214178+scm-sai-zaw-myint@users.noreply.github.com>
2023-04-26 15:35:53 +0630

    Revert "merged and solved error with KAAN branch"
    This reverts commit 59170d634893a9c9134060499dea24b3ffbac54f.

--- ems_mobile/lib/Services/Common/config.dart
+++ ems_mobile/lib/Services/Common/config.dart
@@ -25,9 +25,6 @@ class Config {
   static final String profileChangeHistoryPage = "/profileChangeHistoryPage";
   static final String profileChangeDetailPage = "/profileChangeDetailPage";
 
-  static final String addressChangeRequestPage = "/addressChangeRequestPage";
-  static final String dailyTemperatureReportPage = "/dailyTemperatureReportPage";
-  static final String addressChangeHistoryPage = "/addressChangeHistoryPage";
   /// Pages Routes End ///
 
   /// API Routes  ///
@@ -61,9 +58,6 @@ class Config {
     "4": "Save",
     "7": "Approve (M)"
   };
-
-
-
 
 
 }
